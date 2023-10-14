965cc63cb0276ac34b89800a55d80690aa2d2412
Danish-Mar <danishs2007@gmail.com>
2023-10-14 15:58:24 +0530

    python virtual environme
    	modified:   forms/books_manage.html
    	new file:   server/bin/Activate.ps1
    	new file:   server/bin/activate
    	new file:   server/bin/activate.csh
    	new file:   server/bin/activate.fish
    	new file:   server/bin/flask
    	new file:   server/bin/pip
    	new file:   server/bin/pip3
    	new file:   server/bin/pip3.11
    	new file:   server/bin/python
    	new file:   server/bin/python3
    	new file:   server/bin/python3.11
    	new file:   server/lib/python3.11/site-packages/Jinja2-3.1.2.dist-info/INSTALLER

--- server/main.py
+++ server/main.py
@@ -1,42 +1,48 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, redirect, url_for
+
 import mysql.connector
 
-app = Flask(__name)
+app = Flask(__name__)
 
-@app.route('/add-book', methods=['POST'])
+@app.route('/add-book', methods=['GET','POST'])
 def add_book():
     if request.method == 'POST':
-        # Get book details from the form
-        title = request.form['title']
-        author = request.form['author']
-        publisher = request.form['publisher']
-        isbn = request.form['isbn']
-        price = request.form['price']
-        category = request.form['category']
-        publishYear = request.form['publishYear']
-        scheme = request.form['scheme']
-        source = request.form['source']  # Added source field
-        # Add code to get other book details here
+        try:
+            # Get book details from the form
+            title = request.form['title']
+            author = request.form['author']
+            publisher = request.form['publisher']
+            isbn = request.form['isbn']
+            price = request.form['price']
+            category = request.form['category']
+            publishYear = request.form['publishYear']
+            scheme = request.form['scheme']
+            source = request.form['source']  # Added source field
 
-        # Connect to the MySQL database
-        connection = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='test@123',
-            database='library0'
-        )
+            # Connect to the MySQL database
+            connection = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='test@123',
+                database='library0'
+            )
 
-        cursor = connection.cursor()
+            cursor = connection.cursor()
 
-        # Insert book details into the Books table
-        cursor.execute("INSERT INTO Books (title, authorName, publisher, isbn, price, category, publishYear, source) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                       (title, author, publisher, isbn, price, category, publishYear, source))
+            # Insert book details into the Books table
+            cursor.execute("INSERT INTO Books (title, authorName, publisher, isbn, price, category, publishYear, source) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                           (title, author, publisher, isbn, price, category, publishYear, source))
 
-        # Commit changes and close the database connection
-        connection.commit()
-        connection.close()
+            # Commit changes and close the database connection
+            connection.commit()
+            connection.close()
 
-        return redirect(url_for('success'))
+            return redirect(url_for('success'))
+        except Exception as e:
+            # Handle any exceptions that might occur during the database operation
+            return f"An error occurred: {str(e)}"
+    elif  request.method == 'GET':
+        pass
 
 @app.route('/success')
 def success():
